41cd442eb92edf5f5dedfa0e383df649f08947ee
Connor Slade <connor@connorcode.com>
2024-01-12 23:28:52 -0500

    Main UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,77 @@
 <script lang="ts">
-  import { SimpleGrid, Button, Flex } from "@svelteuidev/core";
+  import { SimpleGrid, Button, Flex, Text } from "@svelteuidev/core";
   import { Symbol } from "radix-icons-svelte";
   import { state } from "./lib/stores";
 
   let words = ["", "", "", ""];
   const refresh = () => (words = $state.randomWords());
+  if ($state.count() != 0) refresh();
 </script>
 
 <main>
-  <SimpleGrid cols={2}>
-    <div class="word">{words[0]}</div>
-    <div class="word">{words[1]}</div>
-    <div class="word">{words[2]}</div>
-    <div class="word">{words[3]}</div>
-  </SimpleGrid>
+  <div class="grid">
+    <SimpleGrid cols={2}>
+      <div class="word">{words[0]}</div>
+      <div class="word">{words[1]}</div>
+      <div class="word">{words[2]}</div>
+      <div class="word">{words[3]}</div>
+    </SimpleGrid>
+  </div>
 
   <div class="footer">
-    <Flex gap={10}>
+    <div class="left">
       <Button on:click={refresh}>
         <Symbol />
       </Button>
-      <Button>Edit Wordlist {$state.count()}</Button>
-    </Flex>
+      <Button>Edit Wordlist ({$state.count()})</Button>
+    </div>
+    <div class="right">
+      <Text>Connor Slade &bull; <a href="/">Source</a></Text>
+    </div>
   </div>
 </main>
 
 <style>
   .word {
+    height: 200px;
+    width: 600px;
+
     font-size: 100px;
     font-family: sans-serif;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 10px;
+    border: 1px solid #ccc;
+  }
+
+  main {
+    height: 100vh;
+  }
+
+  .grid {
+    width: fit-content;
+    position: relative;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
 
   .footer {
+    width: calc(100vw - 30px);
     position: fixed;
     bottom: 0;
-    padding: 20px;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .footer .left,
+  .footer .right {
+    display: flex;
+    align-items: center;
+    gap: 10px;
   }
 </style>
